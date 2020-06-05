d4c793062737fb32e2c0b9f330e3f6419ac26074
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-06-05 15:02:12 -0400

    Connected the App to the API

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,10 +1,85 @@
 import React, { Component } from "react";
-import Search from "./Search";
+import Search from "./SearchField";
 import axios from "axios";
+import ReactDOM from 'react-dom';
 
 class Giphy extends Component {
     constructor(props) {
       super(props);
-     // NOT SURE IF RIGHT this.state = { word: "", giphs: [], searchInput: "" };
-    }
+     // NOT SURE IF RIGHT 
+     this.state = { 
+       word: "", 
+       giphs: [], 
+       searchInput: "Funny cats" };
+    }                  
+  
+
+  componentDidMount(){
+    axios
+    .get("http://api.giphy.com/v1/gifs/search?q=" + this.state.searchInput +"&api_key=" + 
+    process.env.REACT_APP_GIPHY_API_KEY +"&limit=20")
+    .then((response) =>{            
+        this.setState({giphs:response.data.data});
+        let output = []; //output collector
+
+        //map and select query results
+        output = response.data.data.map(gvalue => 
+        <div className="query-result">                
+            <div className="query-data-box">
+                <div className="gif-image">
+                    <img src ={gvalue.images.original.url}/>
+                </div>
+            </div>         
+        </div>);   
+        ReactDOM.render(output,
+          document.getElementsByClassName("results-output")[0]);        
+    })
+    .catch((err) => console.log(err)); //send an error message to the console 
+  }
+
+
+  printGiphs = () => {        
+       
+    let output = []; //output collector
+
+    //map and select query results
+    output = this.state.giphs.map(gvalue => 
+    <div className="query-result">                
+        <div className="query-data-box">
+            <div className="query-data">
+                <div>{console.log(gvalue)}</div>
+            </div>
+        </div>         
+    </div>);       
+    
+    //render query results
+    ReactDOM.render(
+        output,
+        document.getElementsByClassName("results-output")[0]
+    );
+  }
+
+  render()
+  {
+    return(
+        <>
+          <div>           
+            <button
+                        className="button-find"
+                        onClick={() => {
+                            this.componentDidMount();                        
+                        }}
+                        >
+                        Find
+                    </button>
+          </div>
+          <div className="results-output">
+
+          </div>
+          
+        </>
+    );
+  }
 }
+
+export default Giphy;
